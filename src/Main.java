f5a7160bd4c5b5ef8115eb98906fe0ab4eedac7d
Luqman A. Siswanto <l.arifin.siswanto@gmail.com>
2015-11-13 15:52:14 +0700

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 /**
  * Main.java
  */
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args){
-
+        String str = "Hello world!";
+        System.out.println(str);
     }
 }
