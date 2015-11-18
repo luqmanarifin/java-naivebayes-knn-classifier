71c014e2c3f577c4f8d79606b7d07aa7481d9cbc
Luqman A. Siswanto <l.arifin.siswanto@gmail.com>
2015-11-18 20:37:26 +0700

    Add instance

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,32 @@
  * Main.java
  */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
+    static Instance model;
+    static ArrayList<Instance> datasets;
+
+    public static void init() {
+        model = new Instance();
+        model.addAttribute("buying");
+        model.addAttribute("maint");
+        model.addAttribute("doors");
+        model.addAttribute("persons");
+        model.addAttribute("lug_boot");
+        model.addAttribute("safety");
+    }
+
     public static void main(String[] args) {
-        String str = "Hello world!";
-        System.out.println(str);
+        init();
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader("car.data"));
+
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
 }
